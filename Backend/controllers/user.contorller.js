805c018f3cf9ba7899410d6ac0a7b07e6fa8473e
REINKARNATION <iz_in_103.ashcad@izoologic.com>
2024-12-17 18:46:57 +0530

    Committing End of the Day 1/2

--- Backend/controllers/user.contorller.js
+++ Backend/controllers/user.contorller.js
@@ -25,3 +25,14 @@ module.exports.registerUser = async (req, res, next) => {
 
     res.status(201).json({token, user});
 }
+
+module.exports.loginUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email}) 
+}
