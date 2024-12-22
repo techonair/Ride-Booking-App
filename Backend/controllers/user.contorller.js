0d1bcf03fe45f7d0580a0d7bb81d08455bb07d54
Bhanu Pratap <techieonmail@gmail.com>
2024-12-23 00:24:53 +0530

    Basic Authentication for User Model completed | Basic enpoints for users model done

--- Backend/controllers/user.contorller.js
+++ Backend/controllers/user.contorller.js
@@ -2,6 +2,7 @@ const { response } = require('../app');
 const userModel = require('../models/user.model');
 const userService = require("../services/user.services");
 const { validationResult } = require("express-validator");
+const blacklistTokenModel = require('../models/blacklistToken.model');
 
 module.exports.registerUser = async (req, res, next) => {
     const errors = validationResult(req);
@@ -48,9 +49,23 @@ module.exports.loginUser = async (req, res, next) => {
 
     const token = user.generateAuthToken();
 
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 3600000,
+    })
+
     res.status(200).json({token, user});
 }
 
 module.exports.getUserProfile = async (req, res, next) => {
     res.status(200).json(req.user)
+}
+
+module.exports.logoutUser = async (req, res, next) => {
+    res.clearCookie('token');
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+    await blacklistTokenModel.create({token});
+    res.status(200).json({message: 'Logged Out'});
 }
